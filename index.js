214a46b9c6b54ce6d3f2b11f1400050e35c81899
KevinKiseli <kevin.kiseli@student.moringaschool.com>
2023-09-24 11:19:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+{
+
+document.querySelector(main).remove();
+}
+{
+    const newHeader = document.createElement("h1");
+}
+{    document.body.append(newHeader);
+}
+{
+    newHeader.id = "victory";
+}
+{
+        newHeader.id = "victory";
+}
+{        newHeader.inner = "Kevin is the champion";
+}
